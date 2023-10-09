4d3197d4013901aab3ab03e0fb475289492b441c
Sam Lin <maxisam@gmail.com>
2023-10-09 16:53:16 -0500

    test comment

--- dist/index.js
+++ dist/index.js
@@ -805,6 +805,29 @@ exports.log = log;
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -816,7 +839,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.publishComment = void 0;
+const core = __importStar(__nccwpck_require__(2186));
 const github_1 = __nccwpck_require__(5438);
+const util_1 = __nccwpck_require__(3837);
 const markdown_1 = __nccwpck_require__(2519);
 const action_1 = __nccwpck_require__(2216);
 const publishComment = (token, title, message, postNew) => __awaiter(void 0, void 0, void 0, function* () {
@@ -851,6 +876,9 @@ const getExistingComment = (octokit, context, header) => __awaiter(void 0, void 
     var _a;
     const { owner, repo, issueNumber } = context;
     const comments = yield octokit.rest.issues.listComments({ owner, repo, issue_number: issueNumber });
+    core.startGroup('Existing comments');
+    core.info(`${(0, util_1.inspect)(comments)}`);
+    core.endGroup();
     return (_a = comments.data) === null || _a === void 0 ? void 0 : _a.find(comment => {
         var _a, _b;
         const isBotUserType = ((_a = comment.user) === null || _a === void 0 ? void 0 : _a.type) === 'Bot';
